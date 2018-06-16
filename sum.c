a6698da17afc51e2f8f6184b4ee58c31b6d8870a
VrushabBayas <rushab.bayas@gmail.com>
2018-06-16 17:00:24 +0530

    add function

--- sum.c
+++ sum.c
@@ -1,7 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+int addNum(int, int);
 int main()
 {
-	int a = 10, b  = 20;
-	printf("%d",a+b);
+	int a = 10, b = 20;
+	int res = addNum(a, b);
 	return 0;
+}
+
+int addNum(int no1, int no2)
+{
+	return no1 + no2;
 }
