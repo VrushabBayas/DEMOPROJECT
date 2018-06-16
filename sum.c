bd069a1397ead4334b71cec9548e5098577de920
VrushabBayas <rushab.bayas@gmail.com>
2018-06-16 17:13:27 +0530

    sub function used

--- sum.c
+++ sum.c
@@ -4,6 +4,10 @@ int main()
 {
 	int a = 10, b = 20;
 	int res = addNum(a, b);
+	int sub = subtractNum(a, b);
+	printf("Addition is:%d", res);
+	printf("division is:%d", res);
+
 	return 0;
 }
 
@@ -13,5 +17,5 @@ int addNum(int no1, int no2)
 }
 int subtractNum(int no1, int no2)
 {
-        return no1 - no2;
+	return no1 - no2;
 }
